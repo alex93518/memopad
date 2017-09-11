3679a80efe2925e3a005eebafc1e3211bf857222
Jin Jin <jin64480@gmail.com>
2017-09-11 16:50:33 +0800

    backend

--- server/main.js
+++ server/main.js
@@ -1,11 +1,40 @@
 import express from 'express';
 import path from 'path';
+
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
+
+import morgan from 'morgan'; //http request logger
+import bodyParse from 'body-parser';
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+import api from './routes';
+
 
 const app = express();
 const port = 3000;
 const devPort = 4000;
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//mongodb connection
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
+
+/* use session */
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
 
 if(process.env.NODE_ENV == 'development'){
     console.log('Server is running on development mode');
@@ -20,6 +49,20 @@ if(process.env.NODE_ENV == 'development'){
 }
 
 app.use('/', express.static(path.join(__dirname, './../public')));
+
+/* setup routers & static directory */
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './../public/index.html'));
+});
+
+/* handle error */
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 
 app.get('/hello', (req, res) => {
     return res.send('Hello CodeLab');
